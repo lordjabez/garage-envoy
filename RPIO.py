ea24011ef614f15ff567a49ab44f4ec8c0fef4a3
Judson D Neer <judson.neer@gmail.com>
2015-01-30 15:57:19 -0800

    Significant updates to how events are handled.

--- RPIO.py
+++ RPIO.py
@@ -1,3 +1,7 @@
+import fileinput
+import threading
+
+
 BCM = 0
 BOARD = 1
 
@@ -8,29 +12,76 @@ PUD_UP = 0
 PUD_DOWN = 1
 
 
+_pins = {}
+_callbacks = {}
+
+
+_keyinput = input
+
+
+def _setpin(pin, value):
+    _pins[pin] = value
+    _callbacks[pin](pin, value)
+
+
+def _togglepin(pin):
+    _setpin(pin, 0 if _pins[pin] else 1)
+
+
+def _getinputs():
+    while True:
+        code = _keyinput()
+        if code == 'c':
+            _togglepin(24)
+        elif code == 'o':
+            _togglepin(25)
+
+
 def setmode(mode):
     pass
 
 
 def setup(pin, mode, pull_up_down=None):
-    pass
+    if mode == IN:
+        _pins[pin] = 0
 
 
 def input(pin):
-    pass
+    return _pins[pin]
 
 
 def output(pin, state):
-    pass
+    _pins[pin] = state
 
 
-def add_interrupt_callback(pin, mode, debounce_timeout_ms=0):
-    pass
+def add_interrupt_callback(pin, callback, debounce_timeout_ms=0):
+    _callbacks[pin] = callback
 
 
 def wait_for_interrupts(threaded):
-    pass
+    if threaded:
+        threading.Thread(target=_getinputs).start()
+    else:
+        _getinputs()
 
 
 def cleanup():
     pass
+
+
+def _setclosed():
+    _setpin(24, 1)
+
+
+def _setopen():
+    _setpin(25, 1)
+
+
+def _dotrigger():
+    if pin == 17:
+        if _pins.get(24):
+            _togglepin(24)
+        elif _pins.get(25):
+            _togglepin(25)
+        else:
+            threading.Timer(7.5, _setclosed).start()
--- garage-envoy.py
+++ garage-envoy.py
@@ -6,6 +6,7 @@ import argparse
 import json
 import logging
 import logging.handlers
+import threading
 import time
 
 # Additional library imports
@@ -13,11 +14,19 @@ import bottle
 import RPIO
 
 
+# The event log is stored in a flat file.
+EVENT_LOG_FILENAME = 'events.log'
+
+
 # These are logical pin numbers based on the Broadcom
 # chip on the Raspberry Pi model A/B (not the plus).
 TRIGGER_PIN = 17
 OPEN_SENSOR = 25
 CLOSED_SENSOR = 24
+
+# The max time it takes the garage door to open/close in seconds.
+# This value is used to determine door state from sensor events.
+DOOR_TIME = 15.0
 
 
 # Parse the command line parameters.
@@ -37,46 +46,139 @@ else:
     logging.basicConfig(format=logformat, level=loglevel)
 
 
-# Stores all events in memory. Eventually this may be
-# moved to non-volitle storage, but this works for now.
-events = {'open': [], 'closed': [], 'trigger': []}
+thisevent = None
+prevevent = None
 
 
-def readevents(num):
-    pass
+def isrecent(event):
+    recenttime = time.time() - DOOR_TIME
+    return event['time'] > recenttime
 
 
-
-def logevent(name, state=None):
-    """ TODO add comments everywhere """
-    event = {'name': name, 'time': time.time()}
-    if state is not None:
-        event['state'] = state
-    logging.debug(str(event))
-    return event
+def issensor(event):
+    return event['type'] == 'sensor'
 
 
-def logopen(_, state):
-    logevent('open', state == 1)
+def isstate(event):
+    return event['type'] == 'state'
 
 
-def logclosed(_, state):
-    logevent('closed', state == 1)
+def istransient(event):
+    return event['type'] == 'state' and event['name'].endswith('ing')
+
+
+def isopening(event):
+    return event['type'] == 'state' and event['name'] == 'opening'
+
+
+def isclosing(event):
+    return event['type'] == 'state' and event['name'] == 'closing'
+
+
+def ishalfopen(event):
+    return isstate(event) and event['name'] == 'half-open'
+
+
+def ishalfclosed(event):
+    return isstate(event) and event['name'] == 'half-closed'
+
+
+def isopen(event):
+    return issensor(event) and event['name'] == 'open'
+
+
+def isclosed(event):
+    return issensor(event) and event['name'] == 'closed'
+
+
+def istrigger(event):
+    return issensor(event) and event['name'] == 'trigger'
+
+
+def istrue(event):
+    return issensor(event) and event.get('value', False)
+
+
+def readevents(num=0, type=None):
+    with open(EVENT_LOG_FILENAME, 'r') as eventfile:
+        events = [json.loads(line) for line in eventfile]
+    if type:
+        events = [e for e in events if e['type'] == type]
+    return events[-num:]
+
+
+def writeevent(event):
+    with open(EVENT_LOG_FILENAME, 'a') as eventfile:
+        eventfile.write(json.dumps(event) + '\n')
+
+
+def logevent(type, name, value=None):
+    global thisevent
+    global prevevent
+    prevevent = thisevent
+    thisevent = {'time': time.time(), 'type': type, 'name': name, 'value': value}
+    writeevent(thisevent)
+
+
+def logstate():
+    global thisevent
+    global prevevent
+    if isopen(thisevent):
+        if istrue(thisevent):
+            logevent('state', 'open')
+        else:
+            logevent('state', 'closing')
+    elif isclosed(thisevent):
+        if istrue(thisevent):
+            logevent('state', 'closed')
+        else:
+            logevent('state', 'opening')
+    elif istrigger(thisevent):
+        if isclosing(prevevent) or ishalfclosed(prevevent):
+            logevent('state', 'opening')
+        elif isopening(prevevent) or ishalfopen(prevevent):
+            logevent('state', 'closing')
+    elif not isrecent(thisevent):
+        if isopening(thisevent):
+            logevent('state', 'half-open')
+        elif isclosing(thisevent):
+            logevent('state', 'half-closed')
+
+
+def logstatelater():
+    threading.Timer(DOOR_TIME, logstate).start()
+
+
+def handleopen(_, value):
+    logevent('sensor', 'open', value != 0)
+    logstate()
+    logstatelater()
+
+
+def handleclosed(_, value):
+    logevent('sensor', 'closed', value != 0)
+    logstate()
+    logstatelater()
+
+
+def handletrigger():
+    logevent('sensor', 'trigger')
+    logstate()
+    RPIO.output(TRIGGER_PIN, False)
+    time.sleep(0.2)
+    RPIO.output(TRIGGER_PIN, True)
 
 
 @bottle.post('/_trigger')
 def posttrigger():
-    RPIO.output(TRIGGER_PIN, False)
-    time.sleep(0.2)
-    RPIO.output(TRIGGER_PIN, True)
-    logevent('trigger')
+    handletrigger()
 
 
 @bottle.get('/events')
 def getevents():
     num = int(bottle.request.query.get('n') or 0)
-    return readevents(100)
-    #return {'open': events['open'][-n:], 'closed': events['closed'][-n:], 'trigger': events['trigger'][-n:]}
+    type = bottle.request.query.get('t')
+    return {'events': readevents(num, type)}
 
 
 @bottle.get('/')
@@ -92,18 +194,19 @@ RPIO.setmode(RPIO.BCM)
 RPIO.setup(TRIGGER_PIN, RPIO.OUT)
 RPIO.output(TRIGGER_PIN, True)
 
-RPIO.add_interrupt_callback(OPEN_SENSOR, logopen, debounce_timeout_ms=100)
-RPIO.add_interrupt_callback(CLOSED_SENSOR, logclosed, debounce_timeout_ms=100)
+RPIO.add_interrupt_callback(OPEN_SENSOR, handleopen, debounce_timeout_ms=100)
+RPIO.add_interrupt_callback(CLOSED_SENSOR, handleclosed, debounce_timeout_ms=100)
 
 RPIO.setup(OPEN_SENSOR, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 RPIO.setup(CLOSED_SENSOR, RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
-logopen(OPEN_SENSOR, RPIO.input(OPEN_SENSOR))
-logclosed(CLOSED_SENSOR, RPIO.input(CLOSED_SENSOR))
+handleopen(OPEN_SENSOR, RPIO.input(OPEN_SENSOR))
+handleclosed(CLOSED_SENSOR, RPIO.input(CLOSED_SENSOR))
 
 RPIO.wait_for_interrupts(threaded=True)
 
-# Configure and start the webserver
+
+# Configure and start the webserver.
 logging.getLogger('waitress').setLevel(loglevel)
 bottle.run(server='waitress', host='0.0.0.0', port=80, quiet=(not args.debug), debug=args.debug)
 
