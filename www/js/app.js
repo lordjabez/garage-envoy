e8b5814745c0c3af6c5be45dafb9e1cffccf7e09
Judson D Neer <judson.neer@gmail.com>
2015-01-30 16:46:50 -0800

    First cut of user interface.

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,9 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('garageEnvoyApp', ['ionic'])
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -17,3 +12,39 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+.controller('mainCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+
+    $scope.triggerLabel = 'Trigger Door';
+
+    var getStateEvents = function() {
+        $http.get('/events?t=state&n=10')
+            .success(function(data) {
+                $scope.stateEvents = data.events;
+                state = $scope.stateEvents.slice(-1)[0].name;
+                switch (state) {
+                    case 'open':
+                    case 'half-open':
+                        $scope.triggerLabel = 'Close Door';
+                        break;
+                    case 'closed':
+                    case 'half-closed':
+                        $scope.triggerLabel = 'Open Door';
+                        break;
+                    case 'opening':
+                    case 'closing':
+                        $scope.triggerLabel = 'Stop Door';
+                        break;
+                }
+            });
+        $timeout(getStateEvents, 1000);
+    }
+
+    $scope.triggerDoor = function() {
+        $http.post('/_trigger');
+    }
+
+    getStateEvents();
+
+}])
