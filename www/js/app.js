04c5a9fca52ff6e4579abbc087b3d0918c934262
Judson D Neer <judson.neer@gmail.com>
2015-02-04 13:22:39 -0800

    Add comments and formatting to the GUI side of things.

--- www/js/app.js
+++ www/js/app.js
@@ -1,74 +1,86 @@
-
-angular.module('garageEnvoyApp', ['ionic'])
-
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+(function() {
 
 
-.controller('mainCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+  // Always a good idea
+  'use strict';
 
-  var pollTimeout;
 
-  var schedulePoll = function(delay) {
-    $timeout.cancel(pollTimeout);
-    pollTimeout = $timeout(getStateHistory, delay);
-  };
+  // Create the main angular module
+  angular.module('garageEnvoyApp', ['ionic'])
 
-  var getStateHistory = function() {
-    $http.get('/history?n=20')
-      .success(function(data) {
-        $scope.history = data.history;
-        if ($scope.history.length) {
-          $scope.state = $scope.history.slice(-1)[0].name;
-        }
-        else {
-          delete $scope.state;
-        }
-        switch ($scope.state) {
-          case 'open':
-          case 'half-open':
-            $scope.triggerAction = 'Close Door';
-            schedulePoll(5000);
-            break;
-          case 'closed':
-          case 'half-closed':
-            $scope.triggerAction = 'Open Door';
-            schedulePoll(5000);
-            break;
-          case 'opening':
-            $scope.triggerAction = 'Stop Door';
-            schedulePoll(1000);
-            break;
-          case 'closing':
-            $scope.triggerAction = 'Reverse Door';
-            schedulePoll(1000);
-            break;
-          default:
-            schedulePoll(10000);
-        }
-      })
-      .error(function() {
-        delete $scope.history;
-        delete $scope.state;
-        delete $scope.triggerAction;
-        schedulePoll(10000);
-      });
+
+  // This app is simple enough to have only one controller
+  .controller('mainCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+
+    // Hanging on to the timeout object allows it to be cancelled when rescheduled.
+    var pollTimeout;
+
+    // Cancel the previous scheduled poll and make
+    // a new one. The delay value is in milliseconds.
+    var schedulePoll = function(delay) {
+      $timeout.cancel(pollTimeout);
+      pollTimeout = $timeout(getStateHistory, delay);
     };
 
-  $scope.triggerDoor = function() {
-    $http.post('/_trigger');
-    schedulePoll(500);
-  };
+    // Grab the state history from the server.
+    var getStateHistory = function() {
+      $http.get('/history?n=20')
+        .success(function(data) {
+          // Store the history list and latest state (if
+          // it exists, it'll be the last item in the list).
+          $scope.history = data.history;
+          if ($scope.history.length) {
+            $scope.state = $scope.history.slice(-1)[0].name;
+          }
+          else {
+            delete $scope.state;
+          }
+          // Determine what the trigger button will do given the
+          // state. Also vary the rate of the next poll, since if
+          // the door is moving polls should happen more frequently.
+          switch ($scope.state) {
+            case 'open':
+            case 'half-open':
+              $scope.triggerAction = 'Close Door';
+              schedulePoll(5000);
+              break;
+            case 'closed':
+            case 'half-closed':
+              $scope.triggerAction = 'Open Door';
+              schedulePoll(5000);
+              break;
+            case 'opening':
+              $scope.triggerAction = 'Stop Door';
+              schedulePoll(1000);
+              break;
+            case 'closing':
+              $scope.triggerAction = 'Reverse Door';
+              schedulePoll(1000);
+              break;
+            default:
+              schedulePoll(10000);
+          }
+        })
+        // If something goes wrong clear out all previous data.
+        .error(function() {
+          delete $scope.history;
+          delete $scope.state;
+          delete $scope.triggerAction;
+          schedulePoll(10000);
+        });
+    };
 
-  getStateHistory();
+    // Send a trigger message to the server. Since this likely
+    // causes a state change, poll again almost right away.
+    $scope.triggerDoor = function() {
+      $http.post('/_trigger');
+      schedulePoll(500);
+    };
 
-}]);
+    // Get the ball rolling with an initial poll.
+    getStateHistory();
+
+  }]);
+
+
+}());
