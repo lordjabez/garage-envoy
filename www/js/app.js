50a59d7331bd9d04bd10956e4db70f124657e9ab
Judson D Neer <judson.neer@gmail.com>
2015-02-04 09:11:20 -0800

    Rework GUI side to match new backend functions.

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,10 @@ angular.module('garageEnvoyApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
@@ -16,35 +16,54 @@ angular.module('garageEnvoyApp', ['ionic'])
 
 .controller('mainCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
-    $scope.triggerLabel = 'Trigger Door';
+  $scope.triggerLabel = 'Trigger Door';
 
-    var getStateEvents = function() {
-        $http.get('/events?t=state&n=20')
-            .success(function(data) {
-                $scope.stateEvents = data.events;
-                state = $scope.stateEvents.slice(-1)[0].name;
-                switch (state) {
-                    case 'open':
-                    case 'half-open':
-                        $scope.triggerLabel = 'Close Door';
-                        break;
-                    case 'closed':
-                    case 'half-closed':
-                        $scope.triggerLabel = 'Open Door';
-                        break;
-                    case 'opening':
-                    case 'closing':
-                        $scope.triggerLabel = 'Stop Door';
-                        break;
-                }
-            });
-        $timeout(getStateEvents, 1000);
-    }
+  var pollTimeout;
 
-    $scope.triggerDoor = function() {
-        $http.post('/_trigger');
-    }
+  var schedulePoll = function(delay) {
+    $timeout.cancel(pollTimeout);
+    pollTimeout = $timeout(getStateHistory, delay);
+  };
 
-    getStateEvents();
+  var getStateHistory = function() {
+    $http.get('/history?n=20')
+      .success(function(data) {
+        $scope.history = data.history;
+        $scope.state = $scope.history.slice(-1)[0].name;
+        switch ($scope.state) {
+          case 'open':
+          case 'half-open':
+            $scope.triggerAction = 'Close Door';
+            schedulePoll(5000);
+            break;
+          case 'closed':
+          case 'half-closed':
+            $scope.triggerAction = 'Open Door';
+            schedulePoll(5000);
+            break;
+          case 'opening':
+            $scope.triggerAction = 'Stop Door';
+            schedulePoll(1000);
+            break;
+          case 'closing':
+            $scope.triggerAction = 'Reverse Door';
+            schedulePoll(1000);
+            break;
+        }
+      })
+      .error(function() {
+        delete $scope.history;
+        delete $scope.state;
+        delete $scope.triggerAction;
+        schedulePoll(10000);
+      });
+    };
 
-}])
+  $scope.triggerDoor = function() {
+    $http.post('/_trigger');
+    schedulePoll(500);
+  };
+
+  getStateHistory();
+
+}]);
